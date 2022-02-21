0e74648c9f5baa61c42d6ee5c1ce8fd52367faa0
david_smirnov <davidsmidik@gmail.com>
2022-02-21 21:23:18 +0000

    Changed localisation to use .csv

--- Localisation/CSVLoader.cs
+++ Localisation/CSVLoader.cs
@@ -9,7 +9,7 @@ public class CSVLoader
     private TextAsset csvFile;
     private char lineSeperator = '\n';
     private char surround = '"';
-    private string[] fieldSeperator = { "\",\"" };
+    private string[] fieldSeperator = { "," };
 
     public void LoadCSV()
     {
@@ -26,7 +26,6 @@ public class CSVLoader
 
         string[] headers = lines[0].Split(fieldSeperator, System.StringSplitOptions.None);
 
-        for(int i = 0; i < headers.Length; i++)
         {
             if (headers[i].Contains(attributeId))
             {
@@ -37,12 +36,12 @@ public class CSVLoader
 
         Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
 
-        for(int i = 1; i < lines.Length; i++)
+        for (int i = 1; i < lines.Length; i++)
         {
             string line = lines[i];
             string[] fields = CSVParser.Split(line);
 
-            for(int f=0; f < fields.Length; f++)
+            for (int f = 0; f < fields.Length; f++)
             {
                 fields[f] = fields[f].TrimStart(' ', surround);
                 fields[f] = fields[f].TrimEnd(surround);
