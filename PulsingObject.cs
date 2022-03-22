6b79b19bf64dc616fa4aa391afc56a27aee12d5b
david_smirnov <davidsmidik@gmail.com>
2022-03-22 23:16:50 +0000

    Exposed scale for pulsing object

--- PulsingObject.cs
+++ PulsingObject.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PulsingObject : MonoBehaviour
 {
     [SerializeField] private float time = 2f;
+    [SerializeField] private Vector3 targetScale = new Vector3(1.2f, 1.2f, 1.2f);
     private void Start()
     {
         var rect = this.GetComponent<RectTransform>();
-        LeanTween.scale(rect, new Vector3(1.2f, 1.2f, 1.2f), time).setLoopPingPong();
+        LeanTween.scale(rect, targetScale, time).setLoopPingPong();
     }
 }
